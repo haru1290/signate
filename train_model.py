0398f7aaea38c35d54e7854eb4e3ce5eba41d48c
S-Haruya <suzuki@ai.cs.ehime-u.ac.jp>
2022-04-29 20:10:23 +0900

    Update File

--- train_model.py
+++ train_model.py
@@ -1,9 +1,14 @@
 import numpy as np
 import pandas as pd
 import lightgbm as lgb
-from sklearn import datasets
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split, KFold
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error
+
+
+FOLD = 5
+NUM_ROUND = 1000
+VERBOSE_EVAL = 1
 
 
 def make_submit_file(pred):
@@ -12,72 +17,69 @@ def make_submit_file(pred):
     submit.to_csv("./submit.csv", index=None, header=None)
 
 
-def train_model(X_train_cv, X_valid_cv, y_train_cv, y_valid_cv):
-    lgb_train = lgb.Dataset(X_train_cv, y_train_cv, free_raw_data=False)
-    lgb_valid = lgb.Dataset(X_valid_cv, y_valid_cv, reference=lgb_train, free_raw_data=False)
+def get_category_id(category):
+    le = LabelEncoder()
+    category_id = le.fit_transform(category)
 
-    params = {
-        'task': 'train',
-        'boosting_type': 'gbdt',
-        'objective': 'regression',
-        'metric': 'rmse',
-        'learning_rate': 0.1
-    }
+    return category_id
 
-    evaluation_results = {}
-    model = lgb.train(
-        params,
-        lgb_train,
-        num_boost_round=2000,
-        valid_names=['train', 'valid'],
-        valid_sets=[lgb_train, lgb_valid],
-        evals_result=evaluation_results,
-        early_stopping_rounds=10
-    )
 
-    return model
+def lgb_model(params, X, y):
+    valid_scores = []
+    models = []
+    kf = KFold(n_splits=FOLD, shuffle=True, random_state=123)
+
+    for fold, (train_indices, valid_indices) in enumerate(kf.split(X)):
+        X_train, X_valid = X[train_indices], X[valid_indices]
+        y_train, y_valid = y[train_indices], y[valid_indices]
+        lgb_train = lgb.Dataset(X_train, y_train)
+        lgb_eval = lgb.Dataset(X_valid, y_valid)
+
+        model = lgb.train(
+            params,
+            lgb_train,
+            valid_sets=lgb_eval,
+            num_boost_round=NUM_ROUND,
+            verbose_eval=VERBOSE_EVAL
+        )
+
+        y_valid_pred = model.predict(X_valid)
+        score = mean_squared_error(y_valid, y_valid_pred, squared=False)
+        print(f"fold: {fold} | RMSE: {score:.3f}")
+        valid_scores.append(score)
+
+        models.append(model)
+
+    return models
 
 
 def main(): 
     train = pd.read_csv('./data/train.csv')
     test = pd.read_csv('./data/test.csv')
 
-    train_ = train.drop(['id', 'pm25_mid'], axis=1)
-    test_ = test.drop(['id'], axis=1)
-    y_ = train['pm25_mid']
+    categorical_features = ['Country', 'City']
+    for category in categorical_features:
+        train[category] = get_category_id(train[category].values)
+        test[category] = get_category_id(test[category].values)
+
+    X = train.drop(['id', 'pm25_mid'], axis=1).values
+    y = train['pm25_mid'].values
+    X_test = test.drop(['id'], axis=1).values
+    
     
     X_train, X_test, y_train, y_test = train_test_split(
-        train_, y_,
+        X, y,
         test_size=0.2,
-        random_state=123
     )
 
-    # 5分割交差検証
-    models = []
-    round_num = 0
-    K_fold = KFold(n_splits=5, shuffle=True,  random_state=123)
-    for train_cv_idx, valid_cv_idx in K_fold.split(X_train, y_train):
-        X_train_cv = X_train.iloc[train_cv_idx]
-        y_train_cv = y_train.iloc[train_cv_idx]
-        X_valid_cv = X_train.iloc[valid_cv_idx]
-        y_valid_cv = y_train.iloc[valid_cv_idx]
+    params = {
+        'objective': 'regression',
+        'metric': 'rmse',
+        'verbose': -1
+    }
 
-        model = train_model(X_train_cv, X_valid_cv, y_train_cv, y_valid_cv)
-        models.appned(model)
+    models = lgb_model(params, X_train, y_train)
 
-        y_train_pred = model.predict(X_train_cv, num_iteration=model.best_iteration)
-        y_valid_pred = model.predict(X_valid_cv, num_iteration=model.best_iteration)
-        y_test_pred = model.predict(X_test, num_iteration=model.best_iteration)
-
-        train_RMSE_score = mean_squared_error(y_train_cv, y_train_pred, squared=False)
-        valid_RMSE_score = mean_squared_error(y_valid_cv, y_valid_pred, squared=False)
-        test_RMSE_score = mean_squared_error(y_test, y_test_pred, squared=False)
-
-        print(f"train_RMSE:{train_RMSE_score:.3f} | valid_RMSE:{valid_RMSE_score:.3f} | test_RMSE:{test_RMSE_score:.3f}")
-
-        round_num += 1
-    
-    # model = train_model(X_train, X_valid, y_train, y_valid)
     # pred = model.predict(test_)
     # make_submit_file(pred)
 
