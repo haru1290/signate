4e9986bd8d723ca9588755f9b28e71ee8e233217
S-Haruya <suzuki@ai.cs.ehime-u.ac.jp>
2022-05-02 12:09:28 +0900

    Update File

--- train_model.py
+++ train_model.py
@@ -5,7 +5,7 @@ import lightgbm as lgb
 from catboost import CatBoostRegressor
 from sklearn.svm import LinearSVR
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import ElasticNet, LinearRegression
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import KFold
 from sklearn.metrics import mean_squared_error
@@ -76,7 +76,7 @@ class ModelCatBoost:
         params = {
             'iterations': 100000,
             'use_best_model': True,
-            'random_seed': 0,
+            'random_state': 0,
             'l2_leaf_reg': 3,
             'depth': 6,
             'loss_function': 'RMSE',
@@ -106,6 +106,28 @@ class ModelLinearSVR:
             random_state=0,
             epsilon=5.0,
             verbose=100,
+        )
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X):
+        X = self.scaler.transform(X)
+        pred = self.model.predict(X)
+
+        return pred
+
+
+class ModelElasticNet:
+    def __init__(self):
+        self.model = None
+
+    def fit(self, X_train, X_valid, y_train, y_valid):
+        self.scaler = StandardScaler()
+        self.scaler.fit(X_train)
+        X_train = self.scaler.transform(X_train)
+        self.model = ElasticNet(
+            alpha=1,
+            l1_ratio=0.5,
+            verbose=1
         )
         self.model.fit(X_train, y_train)
 
@@ -219,31 +241,36 @@ def main():
     model_1d = ModelLinearSVR()
     pred_train_1d, pred_test_1d = predict_cv(model_1d, X_train_valid, y_train_valid, X_test)
 
-    model_1e = ModelRandomForest()
+    model_1e = ModelElasticNet()
     pred_train_1e, pred_test_1e = predict_cv(model_1e, X_train_valid, y_train_valid, X_test)
+
+    model_1f = ModelRandomForest()
+    pred_train_1f, pred_test_1f = predict_cv(model_1f, X_train_valid, y_train_valid, X_test)
+
 
     print(f'a XGBoost RMSE: {mean_squared_error(y_train_valid, pred_train_1a, squared=False):.4f}')    
     print(f'b LightGBM RMSE: {mean_squared_error(y_train_valid, pred_train_1b, squared=False):.4f}')
     print(f'c CatBoost RMSE: {mean_squared_error(y_train_valid, pred_train_1c, squared=False):.4f}')
     print(f'd LinearSVR RMSE: {mean_squared_error(y_train_valid, pred_train_1d, squared=False):.4f}')
-    print(f'e RandomForest RMSE: {mean_squared_error(y_train_valid, pred_train_1e, squared=False):.4f}')
+    print(f'f ElasticNet RMSE: {mean_squared_error(y_train_valid, pred_train_1e, squared=False):.4f}')
+    print(f'f RandomForest RMSE: {mean_squared_error(y_train_valid, pred_train_1f, squared=False):.4f}')
 
     X_train_2 = pd.DataFrame({
         'pred_1a': pred_train_1a,
         'pred_1b': pred_train_1b,
         'pred_1c': pred_train_1c,
         'pred_1d': pred_train_1d,
-        'pred_1e': pred_train_1e
+        'pred_1e': pred_train_1e,
+        'pred_1f': pred_train_1f
     })
     X_test_2 = pd.DataFrame({
         'pred_1a': pred_test_1a,
         'pred_1b': pred_test_1b,
         'pred_1c': pred_test_1c,
         'pred_1d': pred_test_1d,
-        'pred_1e': pred_test_1e
+        'pred_1e': pred_test_1e,
+        'pred_1f': pred_test_1f
     })
-    X_train_2 = pd.concat([X_train_valid, X_train_2], axis=1)
-    X_test_2 = pd.concat([X_test, X_test_2], axis=1)
 
     model_2b = Model2Linear()
     pred_train_2, pred_test_2 = predict_cv(model_2b, X_train_2, y_train_valid, X_test_2)
